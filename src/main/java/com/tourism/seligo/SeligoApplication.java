38de672efdfce0ea24765b2d0cf1581edc76addc
adheesha0316 <adheeshaanuradha2001@gmail.com>
2025-08-08 21:32:08 +0530

    first commit

---
+++ src/main/java/com/tourism/seligo/SeligoApplication.java
@@ -0,0 +1,13 @@
+package com.tourism.seligo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeligoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeligoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourism/seligo/SeligoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourism.seligo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeligoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
